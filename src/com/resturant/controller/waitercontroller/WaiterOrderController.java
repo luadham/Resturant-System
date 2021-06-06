5d55fb9550bb22752142cfcb16b5bda894f294a1
luadham <luadham@gmail.com>
2021-06-06 21:55:02 +0200

    test

--- src/com/resturant/controller/waitercontroller/WaiterOrderController.java
+++ src/com/resturant/controller/waitercontroller/WaiterOrderController.java
@@ -31,7 +31,6 @@ public class WaiterOrderController {
 
     @FXML
     private void initialize() {
-
         for (Food food : foodList.getListOfFood()) {
             foodListMenu.getItems().add(food.getFoodName());
         }
