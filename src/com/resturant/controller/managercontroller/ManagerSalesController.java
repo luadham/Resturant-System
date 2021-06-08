4947ef6e85755a107e60e911c7e85f96dd0794f6
luadham <luadham@gmail.com>
2021-06-08 03:37:43 +0200

    Edit ViewSales on ManagerSalesController.java

--- src/com/resturant/controller/managercontroller/ManagerSalesController.java
+++ src/com/resturant/controller/managercontroller/ManagerSalesController.java
@@ -4,25 +4,23 @@ import com.resturant.model.Booking;
 import com.resturant.model.BookingList;
 import com.resturant.model.Invoice;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.*;
+
 import java.util.ArrayList;
 
 public class ManagerSalesController {
     @FXML Label totalSales;
     private final BookingList bookingList = BookingList.getBookingList();
     private final ArrayList<Booking> bookings = bookingList.getBookingsList();
-    private double totPrice = 0;
+    private double totalPrice = 0;
 
     @FXML
     private void initialize() {
         for (Booking booking : bookings) {
             Invoice invoice = new Invoice(booking);
-            totPrice += invoice.getOverallPrice();
-            totalSales.setText("Total Income: " + String.valueOf(totPrice));
+            totalPrice += invoice.getOverallPrice();
         }
+        totalSales.setText("Total Income: " + String.valueOf(totalPrice) + " $");
     }
 
 }
